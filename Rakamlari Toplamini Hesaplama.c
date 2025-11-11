f45f379a828b45394c468ffe26910d65c25d788b
JEAN REGIS <240509606@firat.edu.tr>
2025-11-11 22:37:35 +0300

    Add files via upload

---
+++ Rakamlari Toplamini Hesaplama.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5];
+    int sum = 0;
+    float average;
+    
+    printf("5 sayi giriniz:\n");
+    
+    for(int i = 0; i < 5; i++) {
+        printf("%d. sayi: ", i + 1);
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+    
+    average = (float)sum / 5;
+    
+    printf("Girilen sayilarin toplami: %d\n", sum);
+    printf("Girilen sayilarin ortalamasi: %.2f\n", average);
+    
+    return 0;
+}
