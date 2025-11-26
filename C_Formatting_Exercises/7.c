764633f201f9119d879602fe3b06819c1e3183eb
JEAN REGIS <240509606@firat.edu.tr>
2025-11-26 23:49:05 +0300

    Update 7.c

--- C_Formatting_Exercises/7.c
+++ C_Formatting_Exercises/7.c
@@ -4,7 +4,7 @@ int main(void)
 {
     double a=1.0/3.0;
     float b=1.0/3.0;
-    printf("%.6f   %.6f\n",a,b);   //左侧double型 右侧float型
+    printf("%.6f   %.6f\n",a,b);   
     printf("%.12f,   %.12f\n",a,b);
     printf("%.16f,   %.16f\n",a,b);
     printf("DBL_DIG: %d\n",DBL_DIG);
